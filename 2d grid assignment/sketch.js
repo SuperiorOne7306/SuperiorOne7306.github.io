f4b52e35bd7166d875b36b9816c19558b8eec049
SuperiorOne7306 <58191376+SuperiorOne7306@users.noreply.github.com>
2021-02-04 14:10:23 -0600

    continue making the snake length

--- 2d grid assignment/sketch.js
+++ 2d grid assignment/sketch.js
@@ -8,7 +8,12 @@
 let snakeFacing = "down";
 let snakeX = 15;
 let snakeY = 15;
-let generalTime = millis();
+let moved = false;
+let lastMove = 0;
+let moveTime = 200;
+let theSnake = [];
+let snakeCopy;
+let snakeCopy2;
 
 function setup() {
   createCanvas(700, 700);
@@ -19,16 +24,17 @@ function setup() {
   cols = grid[0].length;
   cellWidth = width / cols;
   cellHeight = height / rows;
+
+  theSnake.push[snakeY, snakeX, snakeFacing]
 }
 
 function draw() {
   background(220);
   
-  //moveSnake();
+  moveSnake();
   snakeInGrid();
 
   displayGrid();
-  console.log(generalTime);
 }
 
 function mousePressed() {
@@ -43,7 +49,7 @@ function mousePressed() {
   }
 }
 
-function keyIsPressed() {
+function keyPressed() {
   if (key === "w") {
     snakeFacing = "up";
   }
@@ -62,17 +68,23 @@ function keyIsPressed() {
 function displayGrid() {
   for (let y=0; y<rows; y++) {
     for (let x=0; x<cols; x++) {
+      //empty
       if (grid[y][x] === 0) {
         fill("white");
       }
-      else if (grid[y][x] === 1){
+      //snake head
+      else if (grid[y][x] === 1) {
+        fill("blue");
+      }
+      //snake body
+      else if (grid[y][x] === 2) {
         fill("green");
       }
+      //food
       else if (grid[y][x] === 9) {
-        fill("black")
+        fill("black");
       }
-
-
+      //display
       rect(x*cellWidth, y*cellHeight, cellWidth, cellHeight);
     }
   }
@@ -90,20 +102,39 @@ function createEmptyGrid(cols, rows) {
 }
 
 function snakeInGrid() {
-  grid[snakeY][snakeX] = 1;
-}
+  theSnake[0] = snakeCopy
+  grid[theSnake[0][0]][theSnake[0][1]] = 1;
+  for (let i=1; i<theSnake.length-1; i++) {
+    snakeCopy.push(theSnake[i]);
+    theSnake[i] = snakeCopy[0];
+    grid[theSnake]
+    snakeCopy.splice(0, 1);
+  }
+  grid[oldY][oldX] = 2;
+  }
 
 function moveSnake() {
-  if (snakeFacing === "down") {
-    snakeY += 1;
+  if (!moved) {
+    if (millis() >= lastMove + moveTime) {
+      moved = true
+    }
   }
-  else if (snakeFacing === "up") {
-    snakeY -= 1;
-  }
-  else if (snakeFacing === "left") {
-    snakeX -= 1;
-  }
-  else if (snakeFacing === "right") {
-    snakeX += 1;
+  if (moved) {
+    oldX = snakeX;
+    oldY = snakeY;
+    if (snakeFacing === "down") {
+      snakeY += 1;
+    }
+    else if (snakeFacing === "up") {
+      snakeY -= 1;
+    }
+    else if (snakeFacing === "left") {
+      snakeX -= 1;
+    }
+    else if (snakeFacing === "right") {
+      snakeX += 1;
+    }
+    lastMove = millis();
+    moved = false;
   }
 }
